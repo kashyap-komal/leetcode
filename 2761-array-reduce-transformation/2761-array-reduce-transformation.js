265385a6c5383defa3441eb4a5571a35ff8a54b0
accidental_coder <65062122+kashyap-komal@users.noreply.github.com>
2025-04-02 18:40:17 +0200

    Time: 48 ms (49.71%), Space: 53.6 MB (62.72%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -6,9 +6,12 @@
  */
 var reduce = function(nums, fn, init) {
     let ans=init;
-    for(let i=0;i<nums.length;i++)
-    {
-        ans=fn(ans,nums[i]);
-    }
+    nums.map(num=>{
+        ans=fn(ans,num)
+    })
+    // for(let i=0;i<nums.length;i++)
+    // {
+    //     ans=fn(ans,nums[i]);
+    // }
     return ans;
 };

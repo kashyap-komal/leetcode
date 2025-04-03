d739c3c2704ddc20d0ac840c6400ce33c5511aee
accidental_coder <65062122+kashyap-komal@users.noreply.github.com>
2025-04-03 19:06:57 +0200

    Time: 34 ms (48.88%), Space: 14.9 MB (61.47%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,28 +1,63 @@
 class Solution {
 public:
+    
+    bool isAllStars(string &S1, int i) {
+    for (int j = 1; j <= i; j++) {
+        if (S1[j] != '*')
+            return false;
+    }
+    return true;
+}
+
+// Function to check if S1 matches S2 using wildcard pattern matching
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        if(m==0)
-            return (n==0);
-        bool lookup[n+1][m+1];
-        memset(lookup,false,sizeof(lookup));
-        lookup[0][0]=true;
-        for(int j=1;j<=m;j++)
-            if(p[j-1]=='*')
-                lookup[0][j]=lookup[0][j-1];
+        
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
+
+        dp[0][0]= true;
+         for(int j=1;j<=m;j++)
+        {
+            bool flag=true;
+            for(int k=1;k<=j;k++)
+            {
+                if(p[k-1]!='*')
+                {
+                    flag=false;
+                    break;
+                }
+            }
+            dp[0][j]=flag;
+        }
+        // for(int j=1;j<=m;j++)
+        // {
+        //     dp[0][j]=false;
+        // }
+        // for(int i=1;i<=n;i++)
+        // {
+        //     dp[i][0]=isAllStars(s,i);
+        // }
+
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(p[j-1]=='*')
-                    lookup[i][j]=lookup[i][j-1] || lookup[i-1][j];
-                else if(p[j-1]=='?' || s[i-1]==p[j-1])
-                    lookup[i][j]=lookup[i-1][j-1];
-                else
-                    lookup[i][j]=false;
+                if(s[i-1]==p[j-1] || p[j-1] == '?')
+                {
+                     dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    if(p[j-1]=='*')
+                    dp[i][j]=(dp[i-1][j] || dp[i][j-1]);
+                    else
+                    dp[i][j]=false;
+                }
+                
             }
         }
-        return lookup[n][m];
+        return dp[n][m];
+
+
     }
 };

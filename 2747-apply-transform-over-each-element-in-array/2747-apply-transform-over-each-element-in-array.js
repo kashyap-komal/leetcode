e65bd874409d8b9cef6b8c5f93429e381e9fd2ce
accidental_coder <65062122+kashyap-komal@users.noreply.github.com>
2025-07-31 18:57:20 +0200

    Time: 50 ms (11.56%), Space: 53.9 MB (17.22%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,11 +4,13 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let res=[];
-    for(let i=0;i<arr.length;i++)
-    {
-        res.push(fn(arr[i],i))
-    }
-    return res;
+    // let res=[];
+    // for(let i=0;i<arr.length;i++)
+    // {
+    //     res.push(fn(arr[i],i))
+    // }
+    // return res;
+
+    return arr.map((val,i)=>fn(val,i))
     
 };

3afd5eadf718a22217dbc3fc9a68b05295c3dd50
accidental_coder <65062122+kashyap-komal@users.noreply.github.com>
2025-08-28 23:06:01 +0530

    Time: 209 ms (43.93%), Space: 183.9 MB (37.61%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,42 +1,42 @@
 class Solution {
-    private:
     int timer=1;
 public:
-void dfs(int node, int parent, vector<int> &vis, vector<int> adj[],int tin[], int lowtin[], vector<vector<int>> &bridges)
-{
-    vis[node]=1;
-    tin[node]=lowtin[node]=timer;
-    timer++;
-    for(auto it: adj[node])
-    {
-        if(it == parent )continue;
-        if(vis[it]==0)
+     void dfs(int node,int parent,vector<int>&vis,vector<vector<int>>&adj,vector<int> &tin,vector<int> &lowins,vector<vector<int>>&bridge)
+     {
+        vis[node]=1;
+        tin[node]=lowins[node]=timer;
+        timer++;
+        for(auto it: adj[node])
         {
-            dfs(it,node,vis,adj,tin,lowtin,bridges);
-            lowtin[node]= min(lowtin[node], lowtin[it]);
-
-            if(lowtin[it]>tin[node])
+            if(it==parent)continue;
+            if(vis[it]==0)
             {
-                bridges.push_back({it,node});
+                dfs(it,node,vis,adj,tin,lowins,bridge);
+                lowins[node]=min(lowins[node],lowins[it]);
+                if(lowins[it]>tin[node])
+                {
+                    bridge.push_back({it,node});
+                }
+            }
+            else
+            {
+                lowins[node]=min(lowins[node],lowins[it]);
             }
         }
-        else
-        lowtin[node]= min(lowtin[node], lowtin[it]);
-    }
-}
+     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<int>adj[n];
-        for(auto it: connections)
+            vector<vector<int>> adj(n);
+        for(auto it: connections) //make it 2directional graph
         {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        vector<int> vis(n,0);
-        int tin[n];
-        int lowtin[n];
-        vector<vector<int>>bridges;
-        dfs(0,-1,vis,adj,tin,lowtin,bridges);
-        return bridges;
-
+        vector<int>vis(n,0); //create vis array
+          vector<int> tin(n, -1);
+        vector<int> lowins(n, -1);//adjacnet node lowinsertion of time
+        vector<vector<int>>bridge;
+        dfs(0,-1,vis,adj,tin,lowins,bridge);
+        return bridge ;
+        
     }
 };

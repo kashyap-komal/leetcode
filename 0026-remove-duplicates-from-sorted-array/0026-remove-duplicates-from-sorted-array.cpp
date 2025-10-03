96a35374e0f43fbb4ca705ace4b7ebf0af1a5a5b
accidental_coder <65062122+kashyap-komal@users.noreply.github.com>
2025-10-03 22:59:00 +0530

    Time: 0 ms (100%), Space: 22.7 MB (19.64%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int>s;
-        //first we are putting exah elemtn in hashset then tale the size of size of set ina variable and then again iterate over the set and put all elements from set to array from starting postion
-        for(int i=0;i<nums.size();i++)
+        int i=0;
+      
+        int n=nums.size();
+        for(int j=1;j<n;j++)
         {
-            s.insert(nums[i]);
+            if(nums[i]!=nums[j])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
         }
-        int k=s.size();
-        int j=0;
-        for(auto x:s)
-        {
-            nums[j++]=x;
-        }
-        return k;
+        return i+1;
         
     }
 };
